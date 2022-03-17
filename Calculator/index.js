1024f14b2858391d662726e4662c393c6f6215af
kaushikduttakd <kaushikdutta700@gmail.com>
2022-03-17 22:30:27 +0530

    Calculator

---
+++ Calculator/index.js
@@ -0,0 +1,36 @@
+        function ourAddcalculator()
+        {
+           
+           
+         var num1 = parseInt(document.getElementById("fnumber").value);
+            var num2 = parseInt(document.getElementById("lnumber").value);
+            document.getElementById("res").value = num1 + num2;
+
+        }
+        function ourSubtractcalculator()
+        {
+           
+           
+         var num1 = parseInt(document.getElementById("fnumber").value);
+            var num2 = parseInt(document.getElementById("lnumber").value);
+            document.getElementById("res").value = num1 - num2;
+
+        }
+        function ourMultiplycalculator()
+        {
+           
+           
+         var num1 = parseInt(document.getElementById("fnumber").value);
+            var num2 = parseInt(document.getElementById("lnumber").value);
+            document.getElementById("res").value = num1 * num2;
+
+        }
+        function ourDividecalculator()
+        {
+           
+           
+         var num1 = parseInt(document.getElementById("fnumber").value);
+            var num2 = parseInt(document.getElementById("lnumber").value);
+            document.getElementById("res").value = num1 / num2;
+
+        }
